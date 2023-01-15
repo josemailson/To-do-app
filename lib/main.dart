e0a921dbb3d8861c93356235a05a60b94c52a98b
josemailson <jmailsons@gmail.com>
2023-01-15 19:05:58 -0300

    add: crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:to_do_app/services/injection.dart';
